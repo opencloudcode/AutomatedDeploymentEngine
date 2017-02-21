72dd1bdde612a2aa4b1d3881fd6bdb947bf1cc3d
Edward Douse <edward.douse@outlook.com>
2017-02-21 14:36:58 +0000

    Add Simple servername lookup based on MOFs

--- mtr.ADE.Snippets/DSC/Get-NamesFromMofs.ps1
+++ mtr.ADE.Snippets/DSC/Get-NamesFromMofs.ps1
@@ -2,6 +2,6 @@
 $MofPath   = '.'
 $computers = (get-childitem -Path $MofPath).BaseName.Replace(".meta","") | Get-Unique
 
-Test-DscConfiguration -ComputerName $computers -Path $MofPath
+Test-DscConfiguration     -ComputerName $computers -Path $MofPath
 Set-DscLocalConfiguration -ComputerName $computers -Path $MofPath
-Start-DscConfiguration -ComputerName $computers -Path $MofPath
+Start-DscConfiguration    -ComputerName $computers -Path $MofPath
